ff33adac7309004496818962bda1cf2b1ed63c15
cheng <loveteemo@qq.com>
2019-08-07 10:00:46 +0800

    first

---
+++ src/main/java/com/jxnu/demo/hello.java
@@ -0,0 +1,13 @@
+package com.jxnu.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+
+    @RequestMapping("/hello")
+    public String say(){
+        return "aaa";
+    }
+}
