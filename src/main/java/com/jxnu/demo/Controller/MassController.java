976ae4833eb080d7b724c9d10fad3e27461fb75d
cheng <1026304156@qq.com>
2019-09-03 11:23:47 +0800

    添加了redis

--- src/main/java/com/jxnu/demo/Controller/MassController.java
+++ src/main/java/com/jxnu/demo/Controller/MassController.java
@@ -7,6 +7,7 @@ import com.jxnu.demo.service.MassService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -17,12 +18,14 @@ public class MassController {
     MassService massService;
 
     @RequestMapping("/selectMass")
+    @ResponseBody
     public String selectMass() throws JsonProcessingException {
         List<MassInfo> list=massService.selectMass();
         ObjectMapper mapper=new ObjectMapper();
         String masslist=mapper.writeValueAsString(list);
-
         return masslist;
     }
+
+
 
 }
--- src/main/java/com/jxnu/demo/service/Impl/MassServiceImpl.java
+++ src/main/java/com/jxnu/demo/service/Impl/MassServiceImpl.java
@@ -4,9 +4,12 @@ import com.jxnu.demo.bean.MassInfo;
 import com.jxnu.demo.dao.MassInfoMapper;
 import com.jxnu.demo.service.MassService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 
 @Service
 public class MassServiceImpl implements MassService {
@@ -14,10 +17,17 @@ public class MassServiceImpl implements MassService {
     @Autowired
     private MassInfoMapper MassDao;
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
     @Override
     public List<MassInfo> selectMass() {
         List<MassInfo> list=MassDao.SelectMass();
-
+        redisTemplate.opsForValue().set("test1","反复");
         return list;
     }
+
+
+
+
 }
